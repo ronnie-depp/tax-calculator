8e398a6849708d2efc62a226c61bc1d7ed0e20e5
ronnie-depp <mr.salman.ahmad@gmail.com>
2024-09-02 23:56:09 +0500

    added Users, Remember Tokens & SessionsSchema creation inside (!Schema::hasTable('users')) check.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,143 +20,144 @@ return new class extends Migration
         // Disable Foreign Key Constraints
         Schema::disableForeignKeyConstraints();
 
-        /*
+        
         if (!Schema::hasTable('users')) {
             // The "users" does not table exist...
-        }
-        */
+            
+            Schema::withoutForeignKeyConstraints(function () {
+                // Constraints disabled within this closure...
+                Schema::create('users', function (Blueprint $table) {
 
-        if (Schema::hasTable('users')) {
+                    $table->uuid('id')->primary();//->first()
+                    $table->bigInteger('sort_order')->unique();
+                    $table->string('name');
+                    $table->string('email')->unique();
+                    $table->timestamp('email_verified_at')->nullable();
+                    $table->string('password')->default(Hash::make('P@ssword'));
+                    $table->rememberToken()->nullable();
+                    $table->tinyInteger('is_active');//->default(1)
+                    $table->softDeletes($column = 'deleted_at')->comment('Suspended Role.')->nullable();
+                    $table->timestamps();
+                });
+        
+                // Insert Default SuperAdmin/Author
+                DB::table('users')->insert(
+                    array(
+                        'id' => (string) Uuid::uuid4(),
+                        'sort_order' => 1,
+                        'name' => 'Ronnie DeppleGanger',
+                        'email' => 'mr.salman.ahmad@gmail.com',
+                        'email_verified_at' => NOW(),
+                        'password' => Hash::make('P@ssword'),//md5('P@ssword')
+                        'is_active' => 1,
+                        'created_at' => NOW()
+                    )
+                );
+                DB::table('users')->insert(
+                    array(
+                        'id' => (string) Uuid::uuid4(),
+                        'sort_order' => 2,
+                        'name' => 'Tahreem J. Naseem',
+                        'email' => 'myself@tahreems.com',
+                        'email_verified_at' => NOW(),
+                        'password' => Hash::make('P@ssword'),
+                        'is_active' => 1,
+                        'created_at' => NOW()
+                    )
+                );
+        
+                // Insert Few Agents & Users
+                DB::table('users')->insert(
+                    array(
+                        'id' => (string) Uuid::uuid4(),
+                        'sort_order' => 3,
+                        'name' => 'Cheeku',
+                        'email' => 'Cheeku@Meeku.com',
+                        'email_verified_at' => NOW(),
+                        'password' => Hash::make('P@ssword'),
+                        'is_active' => 1,
+                        'created_at' => NOW()
+                    )
+                );
+                DB::table('users')->insert(
+                    array(
+                        'id' => (string) Uuid::uuid4(),
+                        'sort_order' => 4,
+                        'name' => 'Bumbbu Kaat',
+                        'email' => 'BumbbuKaat@TambbuChaat.com',
+                        'email_verified_at' => NOW(),
+                        'password' => Hash::make('P@ssword'),
+                        'is_active' => 1,
+                        'created_at' => NOW()
+                    )
+                );
+                DB::table('users')->insert(
+                    array(
+                        'id' => (string) Uuid::uuid4(),
+                        'sort_order' => 5,
+                        'name' => 'Ali Hussnain',
+                        'email' => 'huss@nain.com',
+                        'email_verified_at' => NOW(),
+                        'password' => Hash::make('P@ssword'),
+                        'is_active' => 1,
+                        'created_at' => NOW()
+                    )
+                );
+        
+                Schema::create('password_reset_tokens', function (Blueprint $table) {
+                    $table->string('email')->primary();//->first();
+                    $table->string('token');
+                    $table->timestamp('created_at')->nullable();
+                });
+        
+                Schema::create('sessions', function (Blueprint $table) {
+                    /*
+                    $lastSortOrder = DB::table('sessions')->select('*')->orderByDesc('sort_order')->first();
+                    if(isset($lastSortOrder)){
+                        if($lastSortOrder->sort_order() === NULL){
+                            $lastSortOrder = 1;
+                        }
+                        elseif($lastSortOrder->sort_order() !== NULL){
+                            $lastSortOrder = (int) $lastSortOrder->sort_order();
+                        }
+                        else{
+                            dd('Database Logical Error: Recordset not found error!');
+                        }
+                    }
+                    $lastSortOrder = DB::table('sessions')->select('sort_order')->where('sort_order', '<>', NULL)->orderByDesc('sort_order')->first();
+                    //$lastSortOrder = DB::table('sessions')->select('sort_order')->where('sort_order', '=', NULL)->orderByDesc('sort_order')->first();
+                    */
+                    $table->uuid('id')->primary()->default((string) Uuid::uuid4());//->first();
+                    $table->bigInteger('sort_order')->unique();//->default((int) 1);////->toInteger();
+                    $table->foreignId('user_id')->nullable()->index();
+                    $table->string('ip_address', 45)->nullable();
+                    $table->text('user_agent')->nullable();
+                    $table->longText('payload');
+                    $table->integer('last_activity')->index();
+                    // Define Foreign Keys
+                    ////$table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+                    
+                });
+        
+                // Insert Default Session
+                DB::table('users')->insert(
+                    array(
+                        'id' => (string) Uuid::uuid4(),
+                        'sort_order' => 1,
+                        //'user_id' => '8169672b-e182-42f5-bf55-31af5c776c4e',
+                        'payload' => 'mr.salman.ahmad@gmail.com',
+                        'last_activity' => time(),
+                    )
+                );
+
+            });
+
+        }
+       
+
+        /* if (Schema::hasTable('users')) {
             // The "users" table exists...
-        }
-
-        Schema::withoutForeignKeyConstraints(function () {
-            // Constraints disabled within this closure...
-            Schema::create('users', function (Blueprint $table) {
-
-                $table->uuid('id')->primary();//->first()
-                $table->bigInteger('sort_order')->unique();
-                $table->string('name');
-                $table->string('email')->unique();
-                $table->timestamp('email_verified_at')->nullable();
-                $table->string('password')->default(Hash::make('P@ssword'));
-                $table->rememberToken()->nullable();
-                $table->tinyInteger('is_active');//->default(1)
-                $table->softDeletes($column = 'deleted_at')->comment('Suspended Role.')->nullable();
-                $table->timestamps();
-            });
-    
-            // Insert Default SuperAdmin/Author
-            DB::table('users')->insert(
-                array(
-                    'id' => (string) Uuid::uuid4(),
-                    'sort_order' => 1,
-                    'name' => 'Ronnie DeppleGanger',
-                    'email' => 'mr.salman.ahmad@gmail.com',
-                    'email_verified_at' => NOW(),
-                    'password' => Hash::make('P@ssword'),//md5('P@ssword')
-                    'is_active' => 1,
-                    'created_at' => NOW()
-                )
-            );
-            DB::table('users')->insert(
-                array(
-                    'id' => (string) Uuid::uuid4(),
-                    'sort_order' => 2,
-                    'name' => 'Tahreem J. Naseem',
-                    'email' => 'myself@tahreems.com',
-                    'email_verified_at' => NOW(),
-                    'password' => Hash::make('P@ssword'),
-                    'is_active' => 1,
-                    'created_at' => NOW()
-                )
-            );
-    
-            // Insert Few Agents & Users
-            DB::table('users')->insert(
-                array(
-                    'id' => (string) Uuid::uuid4(),
-                    'sort_order' => 3,
-                    'name' => 'Cheeku',
-                    'email' => 'Cheeku@Meeku.com',
-                    'email_verified_at' => NOW(),
-                    'password' => Hash::make('P@ssword'),
-                    'is_active' => 1,
-                    'created_at' => NOW()
-                )
-            );
-            DB::table('users')->insert(
-                array(
-                    'id' => (string) Uuid::uuid4(),
-                    'sort_order' => 4,
-                    'name' => 'Bumbbu Kaat',
-                    'email' => 'BumbbuKaat@TambbuChaat.com',
-                    'email_verified_at' => NOW(),
-                    'password' => Hash::make('P@ssword'),
-                    'is_active' => 1,
-                    'created_at' => NOW()
-                )
-            );
-            DB::table('users')->insert(
-                array(
-                    'id' => (string) Uuid::uuid4(),
-                    'sort_order' => 5,
-                    'name' => 'Ali Hussnain',
-                    'email' => 'huss@nain.com',
-                    'email_verified_at' => NOW(),
-                    'password' => Hash::make('P@ssword'),
-                    'is_active' => 1,
-                    'created_at' => NOW()
-                )
-            );
-    
-            Schema::create('password_reset_tokens', function (Blueprint $table) {
-                $table->string('email')->primary();//->first();
-                $table->string('token');
-                $table->timestamp('created_at')->nullable();
-            });
-    
-            Schema::create('sessions', function (Blueprint $table) {
-                /*
-                $lastSortOrder = DB::table('sessions')->select('*')->orderByDesc('sort_order')->first();
-                if(isset($lastSortOrder)){
-                    if($lastSortOrder->sort_order() === NULL){
-                        $lastSortOrder = 1;
-                    }
-                    elseif($lastSortOrder->sort_order() !== NULL){
-                        $lastSortOrder = (int) $lastSortOrder->sort_order();
-                    }
-                    else{
-                        dd('Database Logical Error: Recordset not found error!');
-                    }
-                }
-                $lastSortOrder = DB::table('sessions')->select('sort_order')->where('sort_order', '<>', NULL)->orderByDesc('sort_order')->first();
-                //$lastSortOrder = DB::table('sessions')->select('sort_order')->where('sort_order', '=', NULL)->orderByDesc('sort_order')->first();
-                */
-                $table->uuid('id')->primary()->default((string) Uuid::uuid4());//->first();
-                $table->bigInteger('sort_order')->unique();//->default((int) 1);////->toInteger();
-                $table->foreignId('user_id')->nullable()->index();
-                $table->string('ip_address', 45)->nullable();
-                $table->text('user_agent')->nullable();
-                $table->longText('payload');
-                $table->integer('last_activity')->index();
-                // Define Foreign Keys
-                ////$table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
-                
-            });
-    
-            // Insert Default Session
-            DB::table('users')->insert(
-                array(
-                    'id' => (string) Uuid::uuid4(),
-                    'sort_order' => 1,
-                    //'user_id' => '8169672b-e182-42f5-bf55-31af5c776c4e',
-                    'payload' => 'mr.salman.ahmad@gmail.com',
-                    'last_activity' => time(),
-                )
-            );
-
-        });
+        } */
         
     }
 
